81fae4b1d3451ee905e5618063a1ff4cc757aad4
Muhammad Umar <umarmuha@protonmail.com>
2024-09-19 13:23:21 -0500

    Ran sentry wizard that added the app to sentry detection

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b12c0e48742df62bf016291f52abf835@o4507975147257856.ingest.us.sentry.io/4507975148765184",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
