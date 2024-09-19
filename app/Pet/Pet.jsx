529c699cda346e5f71999afd3ef486a010dd978b
Muhammad Umar <muhammad_umar@dell.com>
2024-09-18 21:56:41 -0500

    Changed pet details folder name and the route in Pet component Naming both the folders is causing issues sycing across github and different OSes. With both the component folder and the route folder named Pet caused issues. Changed the route from /pet to /petdetails

--- app/Pet/Pet.jsx
+++ app/Pet/Pet.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Pet(props) {
   return (
-    <Link href={`/pet/${props.id}`} className="block">
+    <Link href={`/petdetails/${props.id}`} className="block">
       <div className="rounded-lg border-white">
         <img src={props.images[0]} alt={props.name} />
         <h2>{props.name}</h2>
